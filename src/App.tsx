6acc0e6e11192332370902918a693e8c9ea643d4
Oleksii Morozov <95098796+Oleksii-Morozov@users.noreply.github.com>
2023-12-14 18:46:41 +0200

    [fix] clear local storage on start

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import "./App.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 export const App = () => {
+  localStorage.clear();
   return (
     <>
       <Router>
