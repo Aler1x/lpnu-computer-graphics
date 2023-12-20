b9db22be9a4923473b8509254ffa123535d64cea
naz <nazarii.klymok@botscrew.com>
2023-12-20 02:16:33 +0200

    [lab4] modal

--- src/components/HelpModal/HelpModal.tsx
+++ src/components/HelpModal/HelpModal.tsx
@@ -1,18 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { useHelpModalContext } from "../Sidebar/Sidebar";
+
 interface HelpModalProps {
   setIsOpen: () => void;
   text: string;
   header: string;
 }
 
-const HelpModal = ({ setIsOpen, header, text }: HelpModalProps) => {
+const HelpModal = ({ setIsOpen }: HelpModalProps) => {
+  const currentPage = useHelpModalContext();
+
+  const pageToTexts = [
+    { pageName: "Main", header: "Привіт!", text: "Це головна сторінка." },
+    { pageName: "Fractals", header: "Фрактали", text: "Про фрактали." },
+    { pageName: "Colors", header: "Кольори", text: "Про кольори." },
+    {
+      pageName: "Shapes",
+      header: "Афінні трансформації для руху паралелограма",
+      text: "Для того, щоби надати динаміку паралелограму, заданого трьома його вершинами, ми використовуємо афінні перетворення. Це дозволяє нам ефективно реалізувати його рух за допомогою дзеркального відображення відносно обраної прямої, рівняння якої має вигляд у=ах+b.\n\nВведіть коефіцієнти а і b, щоб побачити, як паралелограм віддзеркалюється та переміщується в просторі.",
+    },
+  ];
+
+  const [currentHeader, setCurrentHeader] = useState("");
+  const [currentText, setCurrentText] = useState("");
+
+  useEffect(() => {
+    const pageValues =
+      pageToTexts.find((p) =>
+        currentPage.toLocaleLowerCase().includes(p.pageName.toLocaleLowerCase())
+      ) || pageToTexts[0];
+    if (!pageValues) return;
+    pageValues.header && setCurrentHeader(pageValues.header);
+    pageValues.text && setCurrentText(pageValues.text);
+  }, [currentPage]);
+
   return (
     <div onClick={setIsOpen}>
-      <div className="fixed inset-0 bg-black bg-opacity-50 z-10" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="fixed inset-0 bg-black bg-opacity-50 z-10"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className="flex items-center justify-center h-full">
-          <div className="flex flex-col items-center bg-[#2c3639] text-[#dcd7c9] p-6 rounded-lg shadow-xl">
-            <h2 className="text-xl font-bold mb-4">{header}</h2>
-            <p className="mb-4">{text}</p>
-            <button className="mt-auto p-4 rounded text-[#2c3639] bg-[#dcd7c9]" onClick={setIsOpen}>
+          <div className="flex flex-col w-1/2 items-center bg-[#2c3639] text-[#dcd7c9] p-8 rounded-lg shadow-xl">
+            <h2 className="text-xl font-bold mb-4 leading-10">
+              {currentHeader}
+            </h2>
+            <p className="mb-4">{currentText}</p>
+            <button
+              className="mt-auto p-4 rounded text-[#2c3639] bg-[#dcd7c9]"
+              onClick={setIsOpen}
+            >
               Закрити
             </button>
           </div>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { SidebarElement } from "./SidebarElement/SidebarElement";
 import { SidebarSeparator } from "../../icons/SidebarSeparator";
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 import HelpModal from "../HelpModal/HelpModal";
 import { shape_header, shape_text } from "../../utils/text";
+
+const HelpModalContext = createContext("");
 
 export const Sidebar = () => {
   const navigate = useNavigate();
@@ -21,12 +23,12 @@ export const Sidebar = () => {
         <SidebarElement
           icon={i}
           onClick={() => {
-            const pageRoute = i === 0 ? '/' : `/${page.toLowerCase()}`;
+            const pageRoute = i === 0 ? "/" : `/${page.toLowerCase()}`;
             navigate(pageRoute);
             setCurrentPage(pageRoute);
           }}
           active={
-            currentPage === '/' && i === 0 ||
+            (currentPage === "/" && i === 0) ||
             currentPage === `/${page.toLowerCase()}`
           }
           isFirstElement={i === 0}
@@ -38,31 +40,37 @@ export const Sidebar = () => {
 
   const onHelpClick = () => {
     setModalIsOpen(true);
-  }
+  };
 
   const closeModal = () => {
     setModalIsOpen(false);
-  }
+  };
 
   return (
-    <div>
-      <aside className="top-0 left-0 z-40 w-28 h-screen transition-transform -translate-x-full sm:translate-x-0">
-        <div className="flex flex-col justify-between h-full px-3 py-4 overflow-y-auto sidebar">
-          <ul className="flex flex-col justify-center items-center space-y-2 font-medium gap-5">
-            {drawSidebar()}
-          </ul>
-          {currentPage === '/shapes' &&
+    <HelpModalContext.Provider value={currentPage}>
+      <div>
+        <aside className="top-0 left-0 z-40 w-28 h-screen transition-transform -translate-x-full sm:translate-x-0">
+          <div className="flex flex-col justify-between h-full px-3 py-4 overflow-y-auto sidebar">
+            <ul className="flex flex-col justify-center items-center space-y-2 font-medium gap-5">
+              {drawSidebar()}
+            </ul>
             <ul className="flex flex-col justify-center items-center space-y-2 font-medium gap-5">
               <SidebarElement icon={4} onClick={onHelpClick} />
             </ul>
-          }
-        </div>
-      </aside>
-      {
-        modalIsOpen && (
-          <HelpModal setIsOpen={closeModal} header={shape_header} text={shape_text} />
-        )
-      }
-    </div>
+          </div>
+        </aside>
+        {modalIsOpen && (
+          <HelpModal
+            setIsOpen={closeModal}
+            header={shape_header}
+            text={shape_text}
+          />
+        )}
+      </div>
+    </HelpModalContext.Provider>
   );
+};
+
+export const useHelpModalContext = () => {
+  return useContext(HelpModalContext);
 };
